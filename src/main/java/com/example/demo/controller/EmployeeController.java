e3970e6f38c9047a74728a9b33f6293b196f306b
星野龍 <ryu0114h@gmail.com>
2022-04-06 03:02:03 +0900

    first commit

---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Employee;
+import com.example.demo.repository.EmployeeRepository;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    private final EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @GetMapping("/employee/{id}")
+    public Optional<Employee> getEmployee(@PathVariable Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    @PostMapping(value = "/employee")
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public void deleteEmployee(@PathVariable Long id) {
+        employeeRepository.deleteById(id);
+    }
+
+}
